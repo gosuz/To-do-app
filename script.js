e29c1851d0cb80fb8eb46b8bab39f90cd1987d89
gosuz <go.suz55@gmail.com>
2024-04-16 20:24:40 +0800

    adde calculations and display of total expected time

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-// 1. Select an element
-  // .querySelector()
-
-  // select each input and the submission button
-// const todo = document.querySelector("#todo");
-// const hr = document.querySelector("#hr");
-// const min = document.querySelector("#min");
-// const addTask = document.querySelector("#addTask");
-
-// 	2. Listen to an event
-// Event listener for each thing -> But you get each thing from listening to the submit button.
-
-// Once you listen to the input, you have to insert it into the list (should include the task, the hrs, minutes)
-
-
-  // .addEventListner("click:() => {}
-
-// 	3. Change the dOM
-
-let total = 0;
+let total_hrs = 0;
+let total_min = 0;
 
 const todo = document.querySelector("#todo");
 const list = document.querySelector("#list");
 const addTask = document.querySelector("#addTask");
-// console.log(todo.value);
+const form = document.querySelector("form");
+// const displayTotal = document.querySelector("#displayTotal");
 
 addTask.addEventListener("click", (event) => {
   // when the submit button is clicked it should either:
@@ -31,36 +14,18 @@ addTask.addEventListener("click", (event) => {
     // console.log("empty task");
   }
   else {
-    // Get the value we got from the user, and add it into the inner HTML (in list)
-    // todo.value -> add it into a list
     let li = document.createElement("li");
     li.innerHTML = todo.value;
-    // // let li = `<li>${todo.value}</li>`;
-    // list.innerHTML += li;
     list.appendChild(li); // <ul> <li>hr min</li> </ul>
 
-    // let time = document.createElement("span");
-    // let hrs = hrs.value;
-    // console.log(hrs.value);
-    // let min = min.value;
-    // console.log(min.value);
-    // You want to create an li
-    // You want to be able to select whatever li element we just created
-
-
-    // Get all the minutes, and add them all up
-  // Convert it into hrs and minutes by dividing total/60 (for hrs) and the remainder to be saved as minutes
-
-  // Get all the hrs
-  // Add all the hrs
-  // And add the remaining hrs from the total mins onto the hrs
-
     let tspan = document.createElement("span");
-    tspan.classList.add("time");
-    let total = `${hrs.value}hrs + ${min.value}min`;
-    tspan.innerHTML = `${hrs.value}hrs ${min.value}min`;
-    li.appendChild(tspan);
+    let hrs = parseInt(form.elements.hrs.value, 10);
+    let min = parseInt(form.elements.min.value, 10);
 
+    tspan.classList.add("time");
+    let total = `${hrs}hrs ${min}min`;
+    tspan.innerHTML = `${hrs}hrs ${min}min`;
+    li.appendChild(tspan);
     //
 
     let span = document.createElement("span");
@@ -69,13 +34,35 @@ addTask.addEventListener("click", (event) => {
     // append the span onto the li
     li.appendChild(span);
 
-    // let total = document.createElement("p");
+    total_hrs += hrs;
+    total_min += min;
 
-    console.log(total);
+    if (total_min >= 60){
+      // You want to update the total hrs and minutes each time you add task
+
+      // Convert it into hrs and minutes
+      let conv_hrs = Math.floor((total_min/60));
+      let conv_min = total_min % 60;
+
+      // then add the hrs into the total_hrs
+      total_hrs = conv_hrs + total_hrs;
+      total_min = conv_min;
+      total = `${total_hrs}hrs ${total_min}min`;
+      // And reassign
+    }
+
+
+    console.log(`This is the total hrs: ${total_hrs}`);
+    console.log(`This is the total min: ${total_min}`);
+    total = `${total_hrs}hrs ${total_min}min`;
+
+    // Only want to update and show the newest one
+    document.getElementById("displayTotal").textContent = total;
   }
   todo.value = '';
   hrs.value = '';
   min.value = '';
+  saveData();
 });
 
 
@@ -83,11 +70,25 @@ addTask.addEventListener("click", (event) => {
 list.addEventListener("click", (event) => {
   if(event.target.tagName === "LI"){
     event.target.classList.toggle("checked");
+    saveData();
   }
   else if(event.target.tagName === "SPAN"){
     event.target.parentElement.remove();
+    saveData();
   }
 });
+
+
+function saveData(){
+  localStorage.setItem("data", list.innerHTML);
+  localStorage.setItem("time", displayTotal.innerHTML);
+}
+
+function display() {
+  list.innerHTML = localStorage.getItem("data");
+  displayTotal.innerHTML = localStorage.getItem("time");
+}
+display();
 
 
 // add event listener for span (x) -> delete item
@@ -101,4 +102,36 @@ list.addEventListener("click", (event) => {
   // but we can use event listener on the click, and make it either create a message saying add something or submitting and showing the task
 
 
-//
+
+/**
+If it was for normal calculations:
+1. Get total hrs
+2. Get total minutes
+
+
+Task:
+1. Get input from user (hrs and minutes)
+let hrs = hrs.value
+let min = min.value
+
+2. Add the total
+2.1 let total_hrs += hrs
+2.2 let total_min += min
+      if total_min >= 60 (convert it into hrs and minutes)
+      Then add hrs to total_hrs       and min to total_min
+
+Either:
+1. get total_min = ((hrs * 60) + min )
+then reconvert this back into total hrs and min when displaying
+
+2. get total=  total_hrs  + total_min
+total_hrs (in hrs)
+toal_min (in min)
+
+then if minute >= 60   convert it into hrs and minutes
+And add them together
+
+
+possibly a loop inside the else statement??
+
+*/
